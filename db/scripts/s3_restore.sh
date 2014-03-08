dc0234e612cd4db08d4fba441ff3c02923a1cd44
Mark Gallop <mark@rollick.id.au>
2014-03-08 09:06:22 +0100

    add lock file for db restore and fix the encrypted restore

--- db/scripts/s3_restore.sh
+++ db/scripts/s3_restore.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+LOCK_FILE="/tmp/db_restore.lck"
+
+if [[ -z LOCK_FILE ]]; then
+  touch LOCK_FILE
+else
+  echo "++ Restore already running"
+  return 0
+fi
+
+touch LOCK_FILE
+
 MONGO_DIR=$1
 MONGORESTORE_PATH="/usr/bin/mongorestore"
 MONGODUMP_PATH="/usr/bin/mongodump"
 MONGO_HOST="localhost"
 MONGO_PORT="27017"
 MONGO_DATABASE="underplan"
+TEMP_DIR="/tmp"
 
 S3_BUCKET_NAME="underplan"
 S3_BUCKET_PATH="mongodb-backups"
@@ -14,7 +26,7 @@ S3_BUCKET_PATH="mongodb-backups"
 BACKUP_URL=`s3cmd ls s3://$S3_BUCKET_NAME/$S3_BUCKET_PATH/* | tail -1 | awk '{print $4}'`
 
 echo "Fetching Backup - $BACKUP_URL"
-cd /tmp
+cd $TEMP_DIR
 
 # Fetch the backup tar file
 s3cmd get --force $BACKUP_URL
@@ -26,21 +38,15 @@ BACKUP_DIR="mongodb-$TIMESTAMP-$HOSTNAME"
 mkdir $BACKUP_DIR && cd $BACKUP_DIR
 # Extract the backup files
 if [[ -z "$MONGO_BACKUP_PASSWD" ]]; then
-  tar -xvf --strip-components 1 $BACKUP_FILENAME
+  tar -xvf --strip-components 1 $TEMP_DIR/$BACKUP_FILENAME
 else
   echo "++ Extracting encrypted backup"
-  openssl enc -in ../$BACKUP_FILENAME -aes-256-cbc -d -k $MONGO_BACKUP_PASSWD | tar --strip-components 1 -xzvf -
+  openssl enc -d -aes-256-cbc -k $MONGO_BACKUP_PASSWD -in $TEMP_DIR/$BACKUP_FILENAME | tar --strip-components 1 --extract --file - --gzip
 fi
 
 if [[ -z "$MONGO_DIR" ]]; then
-  #Force file syncronization and lock writes
-  mongo admin --eval "printjson(db.fsyncLock())"
-
   # Restore backup to running mongodb service
   $MONGORESTORE_PATH -h $MONGO_HOST:$MONGO_PORT --db $MONGO_DATABASE $MONGO_DATABASE --drop
-  
-  #Unlock database writes
-  mongo admin --eval "printjson(db.fsyncUnlock())"
 else
   # Restore backup to shared directory - dropping any existing db files
   $MONGORESTORE_PATH --dbpath $MONGO_DIR --db $MONGO_DATABASE $MONGO_DATABASE --drop
