8db3e0290fe30870d07f53238d5dd9b36d378418
Mark Gallop <mark@rollick.id.au>
2014-03-08 09:13:46 +0100

    use forever for keeping node app up and add -L to curl for settings files in case request is a redirect

--- site/start.sh
+++ site/start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export METEOR_SETTINGS="$(curl $CONFIG_URL)"
+export METEOR_SETTINGS="$(curl -L $CONFIG_URL)"
 export MONGO_URL="mongodb://$DB_PORT_27017_TCP_ADDR:$DB_PORT_27017_TCP_PORT/underplan"
+export UNDERPLAN_PATH="/site-data/underplan"
+# NOTE: set MAIL_URL to enable sending of emails
+# export MAIL_URL="smtp://localhost:25/"
+export PORT=3000
+
+# Create log dir
+mkdir -p /site-data/logs
 
 # Start services
-/usr/bin/supervisord -n
+cd /site-data/underplan/
+forever -a -l /site-data/logs/forever.log -o /site-data/logs/site.log -e /site-data/logs/site_error.log main.js
