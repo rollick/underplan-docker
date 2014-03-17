f75a5ef20d685fc07f5c874414c0cb6c02ab7ffc
Mark Gallop <mark@rollick.id.au>
2014-03-18 00:44:13 +0100

    don't lock db (server sometimes couldn't be unlocked but need a better solution), fix lck file checking when backup running and check whether db is 'ok'

--- db/scripts/s3_backup.sh
+++ db/scripts/s3_backup.sh
@@ -2,17 +2,15 @@
 
 LOCK_FILE="/tmp/db_backup.lck"
 
-if [[ -z LOCK_FILE ]]; then
-  touch LOCK_FILE
+if [ ! -f $LOCK_FILE ]; then
+  touch $LOCK_FILE
 else
-  echo "++ Backup already running"
-  return 0
+  echo "++ Backup already running. Exiting."
+  exit 0
 fi
 
-#Force file syncronization and lock writes
-mongo admin --eval "printjson(db.fsyncLock())"
-
 MONGODUMP_PATH="/usr/bin/mongodump"
+MONGO_PATH="/usr/bin/mongo"
 MONGO_HOST="localhost"
 MONGO_PORT="27017"
 MONGO_DATABASE="underplan"
@@ -20,7 +18,17 @@ MONGO_DATABASE="underplan"
 TIMESTAMP=`date +%F-%H%M`
 S3_BUCKET_NAME="underplan"
 S3_BUCKET_PATH="mongodb-backups"
- 
+
+# Check db is ok
+DB_OK=`$MONGO_PATH $MONGO_DATABASE --eval "printjson(db.stats())" | grep -E 'ok"' | egrep -o '[0-9]+'`
+
+if [[ "$DB_OK" != "1" ]]; then
+  echo "++ $MONGO_DATABASE not ok. Exiting."
+  rm $LOCK_FILE
+
+  exit 1
+fi
+
 cd /tmp
  
 # Create backup
@@ -44,10 +52,7 @@ fi
 s3cmd put $TAR_NAME \
   s3://$S3_BUCKET_NAME/$S3_BUCKET_PATH/$TAR_NAME
 
-#Unlock database writes
-mongo admin --eval "printjson(db.fsyncUnlock())"
-
 # Delete dump and tar files
 rm -Rf $MONGODUMP_NAME* $TAR_NAME
 
-rm LOCK_FILE
+rm $LOCK_FILE
